aaa9b8ffa0623916e832fa2cb52f25312dcbff84
michon92 <128338418+michon92@users.noreply.github.com>
2023-04-27 00:53:40 +0000

    Word Counter in txt file by Eric Matthes

---
+++ Files&Exceptions/words_counter.py
@@ -0,0 +1,17 @@
+def count_words(filename):
+    """obliczanie przybliżonej liczby słów w pliku tekstowym (książce np)"""
+    try:
+        with open(filename, encoding='utf-8') as f:
+            contents = f.read()
+    except FileNotFoundError:
+        #print(f"przepraszamy nie ma takiego pliku {filename}")
+        pass
+    else:
+        words = contents.split()
+        num_words = len(words)
+        print(f"Plik {filename} zawiera {num_words} słów.")
+
+
+filenames = ['alice.txt', 'moby_dick.txt', 'little_women.txt']
+for filename in filenames:
+    count_words(filename)
